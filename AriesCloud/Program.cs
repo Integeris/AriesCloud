7a60fb3bfaf756c7e405ad5e835ac45451537461
Integer <toxaserega14@gmail.com>
2023-10-16 20:53:29 +0300

    Инициализация проекта Window Forms приложения для Desktop

---
+++ AriesCloud/Program.cs
@@ -0,0 +1,20 @@
+﻿using AriesCloud.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace AriesCloud
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
